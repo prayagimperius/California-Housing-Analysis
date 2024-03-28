7757e35483571aa763e8e7862c3249a2da3a30ea
Prayag dutt pandey <prayagduttpandey@Prayags-MacBook-Air-2.local>
2024-03-29 00:49:57 +0530

    Project folders added

---
+++ California Housing Analysis/housing_exp2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+housing_data=pd.read_csv('Housing_Data.csv')
+
+#perform and print exploratory data analysis  on the data
+print(housing_data.describe())
+
+
+
+
